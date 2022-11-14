b53dab43e60418fd5242b6eac878f0fb4b36e620
Tatiana Borda <tatiana0borda@gmail.com>
2022-11-13 21:13:05 -0300

    web3.js dApp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import Web3 from 'web3';
 
 function App() {
+
+  const [account, setAccount] = useState()
+  const [network, setNetwork] = useState()
+  const [balance, setBalance] = useState()
+
+  const web3 = new Web3(Web3.givenProvider || 'https://localhost:8545')
+  
+  useEffect(()=>{
+    loadAccounts()  
+  })
+
+  useEffect(()=>{
+    loadBalance()
+  }, [account])
+
+  async function loadBalance(){
+    const network = await web3.eth.net.getNetworkType()
+    const balance = await web3.eth.getBalance(account, "latest")
+
+    setNetwork(network)
+    setBalance((balance/1e18).toFixed(4))
+  }
+
+  async function loadAccounts(){
+    const accounts = await web3.eth.requestAccounts();
+    setAccount(accounts[0])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Descentralized App 
+        <p>Your account: {account}</p>
+        <p>Your balance ({network}) : {balance}</p>
       </header>
     </div>
   );
